f823e3b80c046057f4e3e8adc63d74d7ff0ab28b
AlexeyBarabash <alexey@brave.com>
2026-03-16 13:52:30 +0200

    [cr148][Android] ToolbarManager.ctor gained ActivityResultTracker and DeviceLockActivityLauncher
    Two new parameters added to ToolbarManager constructor between
    WindowAndroid and OneshotSupplier<ChromeAndroidTask>:
    - ActivityResultTracker
    - DeviceLockActivityLauncher
    
    Chromium change:
    https://source.chromium.org/chromium/chromium/src/+/c0e37322747db4f16956a53ff936c85905593c3b
    
    commit c0e37322747db4f16956a53ff936c85905593c3b
    Author: Sophie Crowley <crowleso@google.com>
    Date:   Wed Mar 11 10:30:11 2026 -0700
    
        [Signin][Android] Add piping for Signin button onclick functionality
    
        This will be immediately followed by its child CL
        https://crrev.com/c/7604273 which uses all of these dependencies in
        adding Seamless Signin + settings onclick functionality for this Signin
        button. Split CL for ease of review. Adapted from
        IdentityDiscController.java
    
        Bug: 478828569
        Change-Id: I600617b391ac061000998b1820ba573a57079e86
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600378
        Commit-Queue: Sophie Crowley <crowleso@google.com>
        Reviewed-by: Liza Bipin <mlbipin@google.com>
        Cr-Commit-Position: refs/heads/main@{#1597857}

--- android/javatests/org/chromium/chrome/browser/BytecodeTest.java
+++ android/javatests/org/chromium/chrome/browser/BytecodeTest.java
@@ -1446,6 +1446,8 @@ public class BytecodeTest {
                         NonNullObservableSupplier.class,
                         OneshotSupplier.class,
                         WindowAndroid.class,
+                        ActivityResultTracker.class,
+                        DeviceLockActivityLauncher.class,
                         OneshotSupplier.class,
                         Supplier.class,
                         NonNullObservableSupplier.class,
